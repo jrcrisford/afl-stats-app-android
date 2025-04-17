463c71b60ebaa4971147415cb61fb2681d6aed56
jrc <jc101@utas.edu.au>
2025-04-17 19:58:25 +1000

    Retrieval of players for stat tracking

--- app/src/main/java/au/edu/utas/jc101/aflstatsapp/MatchTrackingActivity.kt
+++ app/src/main/java/au/edu/utas/jc101/aflstatsapp/MatchTrackingActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import au.edu.utas.jc101.aflstatsapp.databinding.ActivityMatchTrackingBinding
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MatchTrackingActivity : AppCompatActivity() {
     private lateinit var ui: ActivityMatchTrackingBinding
@@ -14,5 +16,33 @@ class MatchTrackingActivity : AppCompatActivity() {
         setContentView(ui.root)
 
         Log.d("NAVIGATION", "Navigated to MatchTrackingActivity")
+
+        val matchId = intent.getStringExtra("matchId")
+        Log.d("DEBUG", "Loaded Match ID: $matchId")
+
+        val db = FirebaseFirestore.getInstance()
+
+        if (matchId != null) {
+            db.collection("matches")
+                .document(matchId)
+                .get()
+                .addOnSuccessListener { document ->
+                    if (document.exists()) {
+                        val playerStats = document.get("playerStats") as? Map<String, Any>
+                        val playerNames = mutableListOf<String>()
+
+                        playerStats?.forEach { (_, playerData) ->
+                            val playerDataMap = playerData as? Map<String, Any>
+                            val playerName = playerDataMap?.get("name") as? String
+                            if (playerName != null) {
+                                playerNames.add(playerName)
+                            }
+                        }
+
+                        Log.d("DEBUG", "Players Loaded: $playerNames")
+                        
+                    }
+                }
+        }
     }
 }
