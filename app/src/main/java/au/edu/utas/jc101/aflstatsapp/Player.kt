fd28bd47c041d616e1368206c94225c5b849989c
jrc <jc101@utas.edu.au>
2025-04-24 17:58:41 +1000

    Comment Player and Team data classes

--- app/src/main/java/au/edu/utas/jc101/aflstatsapp/Player.kt
+++ app/src/main/java/au/edu/utas/jc101/aflstatsapp/Player.kt
@@ -2,17 +2,31 @@ package au.edu.utas.jc101.aflstatsapp
 
 import java.io.Serializable
 
-// Data class representing a player and their statistics
+/**
+ * Data class representing a player in an AFL team and their match stats.
+ *
+ * @property id Unique identifier for the player.
+ * @property name Player's name.
+ * @property number Player's jersey number.
+ * @property team Team the player belongs to.
+ * @property kicks Number of kicks made by the player.
+ * @property handballs Number of handballs made by the player.
+ * @property marks Number of marks taken by the player.
+ * @property tackles Number of tackles made by the player.
+ * @property goals Number of goals scored by the player.
+ * @property behinds Number of behinds scored by the player.
+ * @property actionTimestamps List with timestamps of actions performed by the player.
+ */
 data class Player(
-    val id: String,                                                         // Unique identifier for the player
-    val name: String,                                                       // Player's name
-    val number: Int,                                                        // Player's jersey number
-    val team: String,                                                       // Team the player belongs to
-    var kicks: Int = 0,                                                     // Number of kicks made by the player
-    var handballs: Int = 0,                                                 // Number of handballs made by the player
-    var marks: Int = 0,                                                     // Number of marks taken by the player
-    var tackles: Int = 0,                                                   // Number of tackles made by the player
-    var goals: Int = 0,                                                     // Number of goals scored by the player
-    var behinds: Int = 0,                                                   // Number of behinds scored by the player
-    var actionTimestamps: MutableList<Map<String, Any>> = mutableListOf()   // List with timestamps of actions performed by the player
+    val id: String,
+    val name: String,
+    val number: Int,
+    val team: String,
+    var kicks: Int = 0,
+    var handballs: Int = 0,
+    var marks: Int = 0,
+    var tackles: Int = 0,
+    var goals: Int = 0,
+    var behinds: Int = 0,
+    var actionTimestamps: MutableList<Map<String, Any>> = mutableListOf()
 ) : Serializable
--- app/src/main/java/au/edu/utas/jc101/aflstatsapp/Team.kt
+++ app/src/main/java/au/edu/utas/jc101/aflstatsapp/Team.kt
@@ -2,6 +2,12 @@ package au.edu.utas.jc101.aflstatsapp
 
 import java.io.Serializable
 
+/**
+ * Data class representing a team.
+ *
+ * @property name The name of the team.
+ * @property players A mutable list of players in the team.
+ */
 data class Team(
     var name: String = "",
     var players: MutableList<Player> = mutableListOf()
